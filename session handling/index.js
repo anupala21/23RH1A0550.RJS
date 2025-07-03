3148f7366490f0f79b89489b375c58449d7d9a3a
anupala21 <vaishnavianupala21@gmail.com>
2025-07-03 16:09:12 +0530

    Add files via upload

---
+++ session handling/index.js
@@ -0,0 +1,4 @@
+const mod=require('./modules');
+console.log(mod.pi);
+console.log(mod.add(14,21));
+console.log(mod.add(14,mod.pi));
---
+++ session handling/modules.js
@@ -0,0 +1,4 @@
+const pi=3.14;
+const add=(a,b)=>(a+b);
+module.exports.pi=pi;
+module.exports.add=add;
